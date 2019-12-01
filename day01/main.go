f178266b92813aac99b1172edb74a4daa2315287
Sebastien Descamps <sebastien.descamps@gmail.com>
2019-12-01 21:36:58 +0100

    day1, second part

--- day01/main.go
+++ day01/main.go
@@ -16,6 +16,14 @@ func computeFuel(mass int) int {
 	return fuel
 }
 
+func computeRecursiveFuel(mass int) int {
+	fuel := (mass / 3) - 2
+	if fuel < 0 {
+		return 0
+	}
+	return fuel + computeRecursiveFuel(fuel)
+}
+
 func toi(in string) int {
 	num, err := strconv.Atoi(in)
 	if err != nil {
@@ -25,17 +33,38 @@ func toi(in string) int {
 }
 
 func main() {
-	file, err := os.Open("input.txt")
+	filename := "input.txt"
+	firstPart(filename)
+	secondPart(filename)
+}
+
+func firstPart(filename string) {
+	file, err := os.Open(filename)
 	if err != nil {
 		log.Fatal("Open file failed")
 	}
 	defer file.Close()
-
 	total := 0
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		mass := scanner.Text()
 		total += computeFuel(toi(mass))
+	}
+
+	fmt.Printf("Total %v\n", total)
+}
+
+func secondPart(filename string) {
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal("Open file failed")
+	}
+	defer file.Close()
+	total := 0
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		mass := scanner.Text()
+		total += computeRecursiveFuel(toi(mass))
 	}
 
 	fmt.Printf("Total %v\n", total)
--- day01/main_test.go
+++ day01/main_test.go
@@ -24,5 +24,25 @@ func TestShouldComputeFuelNeeded(t *testing.T) {
 			fmt.Printf("Line:[%v] ok %v\n", data.mass, fuelActual)
 		}
 	}
+}
 
+func TestShouldComputeRecursiveFuelNeeded(t *testing.T) {
+	var massData = []struct {
+		mass         int
+		fuelExpected int
+	}{
+		{12, 2},
+		{14, 2},
+		{1969, 966},
+		{100756, 50346},
+		{3, 0},
+	}
+	for _, data := range massData {
+		fuelActual := computeRecursiveFuel(data.mass)
+		if fuelActual != data.fuelExpected {
+			t.Errorf("Line:[%v] expected %v, actual %v\n", data.mass, data.fuelExpected, fuelActual)
+		} else {
+			fmt.Printf("Line:[%v] ok %v\n", data.mass, fuelActual)
+		}
+	}
 }
